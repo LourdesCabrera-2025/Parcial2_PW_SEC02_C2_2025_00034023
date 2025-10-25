937fd239a654788909952713689e1346b143b157
LourdesCabrera-2025 <00034023@uca.edu.sv>
2025-10-24 19:46:53 -0600

    :memo: finalizando parcial

---
+++ server/controllers/cuentas.controller.js
@@ -0,0 +1,45 @@
+const { json } = require('express');
+const cuentas = require('../data/cuentas.json');
+const { act } = require('react');
+
+exports.listarCuentas = (req, res) => {
+    res.json({
+        count: cuentas.length,
+        data: cuentas
+    });
+};
+
+exports.obtenerCuenta = (req, res) => {
+    const id = parseInt(req.params.id);
+    const cuenta = cuentas.find(c => c.id === id);
+    res.json({
+        finded: !!cuenta,
+        account: cuenta || null
+    });
+};
+
+exports.buscarCuentas = (req , res) => {
+    const { queryParam }  = req.query;
+    let resultados = cuentas.filter (
+        c => 
+            c.id == queryParam  ||
+            c.name.toLowerCase().includes(queryParam.toLowerCase()) ||
+            c.gender.toLowerCase() === queryParam.toLowerCase() 
+    );
+
+    if(resultados.length === 1) 
+        res.json ({ finded: true, account: resultados[0]});
+    else if (resultados.length > 1)
+        res.json ({ finded: true, data: resultados});
+    else
+        res.json ({ finded: false});
+}
+
+exports.obtenerBalanceTotal = (req, res) => {
+    const activas = cuentas.filter(c => c.isActive);
+    const total = activas.reduce ((sum, c) => sum + c.balance, 0);
+    res.json({
+        status: activas.length > 0,
+        accountBalance: total
+    });
+};
